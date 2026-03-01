95c7da457cc6d9593741ef3b28738707ac9f82e9
jocim-nix <joe.susatiyo@gmail.com>
2026-03-01 13:40:53 +0700

    fix: api url uses .env

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://forms-api.224668.xyz/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
